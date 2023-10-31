6349a12eee8fa446aa717f9503934c4eb44c1b8f
LuciAkirami <ajsubscriber001@gmail.com>
2023-10-31 20:20:20 +0530

    Created Basic Axum Server

--- axum-p1/src/main.rs
+++ axum-p1/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+
+// The entry point of the program, an async main function.
+#[tokio::main]
+async fn main() {
+    // Create an Axum router for defining HTTP routes and their handlers.
+    let routes = Router::new().route("/", get(|| async { Html("<p>Henlo world</p>") }));
+
+    // Define the network address (IP and port) to bind the server to.
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+
+    // Create an Axum server and start serving the defined routes on the specified address.
+    axum::Server::bind(&addr)
+        .serve(routes.into_make_service())
+        .await
+        .unwrap();
 }
