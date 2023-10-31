190ec213befe49f56166da6b623aff68fc21e25b
LuciAkirami <ajsubscriber001@gmail.com>
2023-10-31 20:49:54 +0530

    added handler func and test

--- axum-p1/src/main.rs
+++ axum-p1/src/main.rs
@@ -1,19 +1,27 @@
 use std::net::SocketAddr;
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 
 // The entry point of the program, an async main function.
 #[tokio::main]
 async fn main() {
     // Create an Axum router for defining HTTP routes and their handlers.
-    let routes = Router::new().route("/", get(|| async { Html("<p>Henlo world</p>") }));
+    let routes = Router::new().route("/hello", get(handle_request));
 
     // Define the network address (IP and port) to bind the server to.
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
     // Create an Axum server and start serving the defined routes on the specified address.
     axum::Server::bind(&addr)
         .serve(routes.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handle_request() -> impl IntoResponse {
+    Html("<p>Henlo Warudoo</p>")
 }
